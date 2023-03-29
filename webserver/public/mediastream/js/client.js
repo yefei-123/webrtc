3aeaba0ef54eb86c607ff0e6be467caa066cba2d
yf <1804955507@qq.com>
2023-03-29 17:59:36 +0800

    video audio strict

--- webserver/public/mediastream/js/client.js
+++ webserver/public/mediastream/js/client.js
@@ -1,26 +1,84 @@
 'use strict'
+
+var audioSource = document.querySelector('select#audioSource');
+var audioOutput = document.querySelector('select#audioOutput');
+var videoSource = document.querySelector('select#videoSource');
+
 var videoplay = document.querySelector('video#player');
+
 function getMediaStream(stream)
 {
-	videoplay.srcObject = stream;
+	videoplay.srcObject  =  stream;
+	return navigator.mediaDevices.enumerateDevices();
 }
-function handleError(error)
+function getDeviceInforsucess(deviceInfos)
 {
-	console.log('getUserMedia error:',error);
+	deviceInfos.forEach(function (deviceInfo)
+	{
+		console.log(deviceInfo.kind 
+			+ ": label=" + deviceInfo.label 
+			+ ": id=" + deviceInfo.deviceId 
+			+ ": groupId=" + deviceInfo.groupId);
+
+	var option = document.createElement('option');
+	option.text = deviceInfo.label;
+	option.value = deviceInfo.deviceId;
+	if(deviceInfo.kind === 'audioinput')
+	{
+		console.log("kind:"+deviceInfo.kind);
+		audioSource.appendChild(option);
+	}
+	else if(deviceInfo.kind === 'audiooutput')
+	{
+		audioOutput.appendChild(option);
+
+	}
+	else if(deviceInfo.kind === 'videoinput')
+	{
+		videoSource.appendChild(option);
+
+	}
+	});
+	
 }
-if(!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia)
+
+function getDeviceInforfail(err)
 {
-	console.log("getUserMedia is not supported");
+	console.log(err.name + " : " + err.message);
+	
 }
-else
+
+
+function  start()
 {
-	var constrants = {
-		video : {
-			height:1080,
-			wight:1920,
-			frmate:30
-		},
-		audio : true
-	};
-	navigator.mediaDevices.getUserMedia(constrants).then(getMediaStream).catch(handleError);
+	if(!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia)
+	{
+		console.log("不支持 mediaDevices或者getUserMedia");
+		return;
+	}
+	else
+	{
+		var deviceId = videoSource.value;
+		var constrants = {
+			video :{
+				weight:1920,
+				height:1080,
+				facingMode:'enviroment',
+				deviceId: deviceId ? deviceId : undefined
+				
+			},
+			audio:{
+				volume:10,
+				echoCancellation:true
+			}
+		}; 
+		navigator.mediaDevices.getUserMedia(constrants)
+			.then(getMediaStream)
+			.then(getDeviceInforsucess)
+			.catch(getDeviceInforfail);
+	}
 }
+
+start();
+
+videoSource.onchange = start;
